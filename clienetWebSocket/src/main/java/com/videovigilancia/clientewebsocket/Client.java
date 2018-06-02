f7f7d72537a343ef349cbdf3e769c19eda354fce
alex yungan <yungan@hotmail.es>
2018-06-02 04:44:52 -0500

    app cliente wesocket

--- clienetWebSocket/src/main/java/com/videovigilancia/clientewebsocket/Client.java
+++ clienetWebSocket/src/main/java/com/videovigilancia/clientewebsocket/Client.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.videovigilancia.clienetwebsocket;
+package com.videovigilancia.clientewebsocket;
 
 import com.github.sarxos.webcam.Webcam;
 import java.awt.image.BufferedImage;

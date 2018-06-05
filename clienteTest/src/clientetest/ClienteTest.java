cd3d54e9698b5556eeed459ecbc6134d931022f6
alex yungan <yungan@hotmail.es>
2018-06-05 10:23:41 -0500

    app

--- clienteTest/src/clientetest/ClienteTest.java
+++ clienteTest/src/clientetest/ClienteTest.java
@@ -9,8 +9,12 @@ import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
 import com.github.sarxos.webcam.ds.vlcj.VlcjDriver;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import java.util.Arrays;
+import java.util.List;
+import uk.co.caprica.vlcj.medialist.MediaListItem;
 
 /**
  *
@@ -18,17 +22,29 @@ import javax.swing.JFrame;
  */
 public class ClienteTest {
 
+    private static List<MediaListItem> EMPTY = new ArrayList<MediaListItem>();
+
+    /* NOTE!
+     * 
+     * The vlclib does not implement video device discovery on Windows. 
+     * Therefore, to make it working on this operating system one needs
+     * to manually provide the list of media list items from vlcj. This
+     * is not necessary on Linux and Mac.
+     */
+    private static final MediaListItem dev0 = new MediaListItem("HP HD Webcam [Fixed]", "dshow://", EMPTY);
+    private static final MediaListItem dev1 = new MediaListItem("USB2.0 Camera", "dshow://", EMPTY);
+    private static final MediaListItem dev2 = new MediaListItem("Logitech Webcam", "dshow://", EMPTY);
+
     static {
-        Webcam.setDriver(new VlcjDriver());
+        Webcam.setDriver(new VlcjDriver(Arrays.asList(dev0, dev1, dev2)));
     }
 
-    public static void main(String[] args)   {
-
-        JFrame frame = new JFrame("GStreamer Webcam Capture Driver Demo");
-	frame.add(new WebcamPanel(Webcam.getDefault()));
-	frame.pack();
-	frame.setVisible(true);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Webcam Panel");
+        window.add(new WebcamPanel(Webcam.getDefault()));
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.pack();
+        window.setVisible(true);
     }
 }

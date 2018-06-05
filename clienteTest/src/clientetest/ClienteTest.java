898184725e568e32b90221d96f171becabdad3bf
alex yungan <yungan@hotmail.es>
2018-06-05 11:35:14 -0500

    app

--- clienteTest/src/clientetest/ClienteTest.java
+++ clienteTest/src/clientetest/ClienteTest.java
@@ -7,13 +7,8 @@ package clientetest;
 
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
-import com.github.sarxos.webcam.WebcamResolution;
-import com.github.sarxos.webcam.ds.gstreamer.GStreamerDriver;
-import com.github.sarxos.webcam.ds.gstreamer.ScreenCaptureDriver;
-import com.github.sarxos.webcam.ds.openimaj.OpenImajDriver;
+import com.github.sarxos.webcam.ds.civil.LtiCivilDriver;
 import com.github.sarxos.webcam.ds.vlcj.VlcjDriver;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
@@ -40,36 +35,16 @@ public class ClienteTest {
     private static final MediaListItem dev0 = new MediaListItem("HP HD Webcam [Fixed]", "dshow://", EMPTY);
     private static final MediaListItem dev1 = new MediaListItem("USB2.0 Camera", "dshow://", EMPTY);
     private static final MediaListItem dev2 = new MediaListItem("Logitech Webcam", "dshow://", EMPTY);
-//https://svn.code.sf.net/p/mjpg-streamer/code/ mjpg-streamer-code
 
     static {
-        //  Webcam.setDriver(new VlcjDriver(Arrays.asList(dev0, dev1, dev2)));
-        Webcam.setDriver(new ScreenCaptureDriver());
+       Webcam.setDriver(new LtiCivilDriver());
     }
 
     public static void main(String[] args) {
-        final JFrame window = new JFrame("Screen Capture Example");
-        window.setResizable(true);
+        JFrame window = new JFrame("Webcam Panel");
+        window.add(new WebcamPanel(Webcam.getDefault()));
+        window.setResizable(false);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.getContentPane().setLayout(new FlowLayout());
-
-        final Dimension resolution = WebcamResolution.QVGA.getSize();
-
-        for (final Webcam webcam : Webcam.getWebcams()) {
-
-            webcam.setCustomViewSizes(resolution);
-            webcam.setViewSize(resolution);
-            webcam.open();
-
-            final WebcamPanel panel = new WebcamPanel(webcam);
-            panel.setFPSDisplayed(true);
-            panel.setDrawMode(WebcamPanel.DrawMode.FIT);
-            panel.setImageSizeDisplayed(true);
-            panel.setPreferredSize(resolution);
-
-            window.getContentPane().add(panel);
-        }
-
         window.pack();
         window.setVisible(true);
     }
